5b58c15e7592566b0fc43a60c73905018646465d
lfcyja <cyjada@bluewin.ch>
2024-12-20 09:18:22 +0100

    introduce helper functions to render razor fragments

---
+++ Galaxus.Functional.Blazor/OptionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Galaxus.Functional.Blazor;
+
+public static class OptionExtensions
+{
+    public static RenderFragment RenderIfSome<T>(
+        this Option<T> value,
+        Func<T, RenderFragment> content)
+    {
+        return value.IsSome
+            ? builder => content(value.Unwrap())(builder)
+            : _ => { }; // Return empty fragment if condition is false
+    }
+
+    public static RenderFragment RenderMatch<T>(
+        this Option<T> value,
+        Func<T, RenderFragment> onOptionIsSomeFragment,
+        Func<RenderFragment> onOptionIsNoneFragment)
+    {
+        return value.IsSome
+            ? builder => onOptionIsSomeFragment(value.Unwrap())(builder)
+            : builder => onOptionIsNoneFragment()(builder);
+    }
+}
